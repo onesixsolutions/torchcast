5a05a2ed0e932ccc1b4f95ec5e5aa8279bc4eb00
Jacob <jacob.dink@strong.io>
2026-05-01 16:03:55 -0500

    add SaturatedLinearModel to process top level

--- torchcast/process/__init__.py
+++ torchcast/process/__init__.py
@@ -6,11 +6,12 @@
 * :class:`.Season` - a process with seasonal structure, implementing the fourier-series based model from
   `De Livera, A.M., Hyndman, R.J., & Snyder, R. D. (2011)`.
 * :class:`.LinearModel` - a linear-model allowing for external predictors.
+* :class:`.SaturatedLinearModel` - a linear model that allows for saturation effects (via EKF).
 
 ----------
 """
 
 from .process import Process
-from .regression import LinearModel
+from .regression import LinearModel, SaturatedLinearModel
 from .local import LocalLevel, LocalTrend
 from .season import Season
